cd65d0f7ef9f2e0cc5f7d83ae953fe85402f5799
Thomas Reynaud <thomas@unbabel.com>
2019-04-21 22:57:34 +0100

    Update master ahead from new-branch

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Infos
 
 This is a README. I try to learn to use git correctly with vim.
+This is another conflict!
--- file1.py
+++ file1.py
@@ -1,5 +1,7 @@
 # This is a python file.
 
+print("This is a conflict with the branch")
 print("Hello world")
 a = 2
 a = 3
+f = 2
